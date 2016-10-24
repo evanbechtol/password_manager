1989bafd4e2ee9641967ee064660a8d34bb71b0c
Evan Bechtol <evanclaybechtol@gmail.com>
2016-10-24 13:22:43 -0500

    Refactored commands into own file

--- app.js
+++ app.js
@@ -1,42 +1,6 @@
 var storage = require('node-persist');
 storage.initSync();
-
-var argv = require('yargs')
-    .command('create', 'Create an account', function (yargs) {
-        yargs.options({
-            name: {
-                demand: true,
-                alias: 'n',
-                description: 'Name of account e.g.) Facebook',
-                type: 'string'
-            },
-            username: {
-                demand: true,
-                alias: 'u',
-                description: 'Username that is associated with the account',
-                type: 'string'
-            },
-            password: {
-                demand: true,
-                alias: 'p',
-                description: 'Password that is used for the given account and username',
-                type: 'string'
-            }
-        });
-    })
-    .command('get', 'Retrieve an account', function (yargs) {
-        yargs.options({
-            name: {
-                demand: true,
-                alias: 'n',
-                description: 'Name of account to retrieve',
-                type: 'string'
-            }
-        });
-    })
-    .help('help')
-    .argv;
-var command = argv._[0];
+var commands = require(__dirname + '/commands.js');
 
 console.log('Starting password manager');
 
@@ -66,15 +30,15 @@ function getAccount (accountName) {
     return matchedAccount;
 }
 
-if (command === 'create') {
+if (commands.command === 'create') {
     var createdAccount = createAccount({
-        name: argv.name,
-        username: argv.username,
-        password: argv.password
+        name: commands.argv.name,
+        username: commands.argv.username,
+        password: commands.argv.password
     });
     console.log('Created account: \n' + JSON.stringify(createdAccount));
-} else if (command === 'get') {
-    var retrievedAccount = getAccount(argv.name);
+} else if (commands.command === 'get') {
+    var retrievedAccount = getAccount(commands.argv.name);
 
     if (typeof retrievedAccount === 'undefined') {
         console.log('Account not found with that name');
---
+++ commands.js
@@ -0,0 +1,41 @@
+var argv = require('yargs')
+    .command('create', 'Create an account', function (yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Name of account e.g.) Facebook',
+                type: 'string'
+            },
+            username: {
+                demand: true,
+                alias: 'u',
+                description: 'Username that is associated with the account',
+                type: 'string'
+            },
+            password: {
+                demand: true,
+                alias: 'p',
+                description: 'Password that is used for the given account and username',
+                type: 'string'
+            }
+        });
+    })
+    .command('get', 'Retrieve an account', function (yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Name of account to retrieve',
+                type: 'string'
+            }
+        });
+    })
+    .help('help')
+    .argv;
+var command = argv._[0];
+
+module.exports = {
+    argv: argv,
+    command: command
+};
