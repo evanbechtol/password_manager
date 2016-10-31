fbe2d2dabc97d609cc8a14a167e8d14e1e23a921
Evan Bechtol <evanclaybechtol@gmail.com>
2016-10-31 15:53:10 -0500

    Added todo: replace AES encryption with key to hashing algorithm for data transfer to server

--- client/js/factories/accountfactory.js
+++ client/js/factories/accountfactory.js
@@ -1,5 +1,6 @@
 var app = angular.module('pwmgr');
-
+/* TODO: Add masterPassword as field to send in all requests
+ * TODO: Replace encryption by AES with hashing algorithm. */
 app.factory('accountFactory', ['$q', '$timeout', '$http', '$cookies', '$rootScope', function ($q, $timeout, $http, $cookies, $rootScope) {
     var factory = {};
     var accountData = {};
@@ -27,7 +28,23 @@ app.factory('accountFactory', ['$q', '$timeout', '$http', '$cookies', '$rootScop
 
 
     factory.get = function () {
+        var deferred = $q.defer();
+        var encryptedAccount = {};
+        encryptedAccount.name = CryptoJS.AES.encrypt(account.name, '!@_pr3Ssur3C0ok_ER!').toString();
+        encryptedAccount.username = CryptoJS.AES.encrypt(account.username, '!@_pr3Ssur3C0ok_ER!').toString();
+        encryptedAccount.masterPassword = CryptoJS.AES.encrypt(account.masterPassword, '!@_pr3Ssur3C0ok_ER!').toString();
 
+        $http.post('/account/get', encryptedAccount)
+            .then(function (data) {
+                if (data.status === 200) {
+                    deferred.resolve('Account created successfully!');
+                } else {
+                    deferred.reject('Error creating account: ' + data);
+                }
+            }, function (err) {
+                deferred.reject(err);
+            });
+        return deferred.promise;
     };
 
 
